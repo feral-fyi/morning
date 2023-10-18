b18320aaa27f35402cb684a4b0583453f7e2e243
Roo Williams <roo@roowilliams.com>
2023-10-18 21:23:17 +0100

    add cms

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head"
+import Script from "next/script"
+import Header from "@components/Header"
+import Footer from "@components/Footer"
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Morning!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <Script id="netlify">
+        {`
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        `}
+      </Script>
     </div>
   )
 }
